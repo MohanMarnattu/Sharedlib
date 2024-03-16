92f350fe80da3b35b9465d5355a59731fbc900f3
MohanMarnattu <100912825+MohanMarnattu@users.noreply.github.com>
2024-03-16 17:32:27 +0530

    Update share.groovy

--- vars/share.groovy
+++ vars/share.groovy
@@ -8,3 +8,7 @@ def mavenBuild(){
 def qadeploy(jobname,ip,path){
      sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${path}.war"
 }
+
+def sh(jobaname){
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
