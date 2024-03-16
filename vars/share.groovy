2cce3a193595d0d731871db746fbf2b2364a78e2
MohanMarnattu <100912825+MohanMarnattu@users.noreply.github.com>
2024-03-16 16:43:16 +0530

    Update share.groovy

--- vars/share.groovy
+++ vars/share.groovy
@@ -2,9 +2,9 @@ def gitDownload(repo){
   git "https://github.com/intelliqittrainings/${repo}.git"
 }
 def mavenBuild(){
-  sh 'mvn package'
+  sh "mvn package"
 }
 
 def qadeploy(jobname,ip,path){
-     sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.jar ubuntu@${ip}:/var/lib/tomcat9/webapps/${path}.war'
+     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.jar ubuntu@${ip}:/var/lib/tomcat9/webapps/${path}.war"
 }
